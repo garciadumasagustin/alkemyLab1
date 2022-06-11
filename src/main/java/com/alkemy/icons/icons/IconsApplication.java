f04a29b09bf7a25ae92455938c643127b86b824b
agustin <agustingarciadumas9@gmail.com>
2022-06-11 17:15:51 -0300

    Initial commit

---
+++ src/main/java/com/alkemy/icons/icons/IconsApplication.java
@@ -0,0 +1,14 @@
+package com.alkemy.icons.icons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootApplication
+public class IconsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IconsApplication.class, args);
+	}
+
+}
